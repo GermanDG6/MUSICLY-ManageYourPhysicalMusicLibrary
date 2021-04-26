bc6b011d08c32a6ac0738f76c1448dabb6c7e192
GermanDG6 <germandg6@gmail.com>
2021-04-26 10:27:09 +0100

    added new interactions with the lists

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const Album = require('../models/Album.model');
 const router = express.Router();
 
-
+const Album = require('../models/Album.model');
 const User = require('../models/User.model');
+
 
 //Middleware de checkForAuth
 const checkForAuth = (req,res,next) => {
@@ -27,11 +27,18 @@ router.get('/my-list', checkForAuth ,(req, res) => {
     console.log(err)
   });
 })
-
 router.post('/my-list/:_id', checkForAuth,(req,res,next)=>{
   User.findById(req.user._id)
   .then((result) => {
-    if(!result.myList.includes(req.params._id)){
+    if(!result.myList.includes(req.params._id) && result.wishList.includes(req.params._id)){
+      User.findByIdAndUpdate(req.user._id, {$pull: {wishList: req.params._id}})
+      .then((result) => {
+        User.findByIdAndUpdate(req.user._id,{$push: {myList: req.params._id}})
+        .then((result) => {
+          res.redirect('/profile/my-list')
+        })
+      })
+    }else if(!result.myList.includes(req.params._id)){
       User.findByIdAndUpdate(req.user._id, {$push: {myList: req.params._id}})
         .then((result) => {
           res.redirect('/profile/my-list')
@@ -44,7 +51,6 @@ router.post('/my-list/:_id', checkForAuth,(req,res,next)=>{
     console.log(err)
   });
 });
-
 router.post('/my-list/:_id/delete', checkForAuth,(req,res)=>{
   User.findByIdAndUpdate(req.user._id, {$pull: {myList: req.params._id}})
   .then((result) => {
@@ -54,10 +60,6 @@ router.post('/my-list/:_id/delete', checkForAuth,(req,res)=>{
       console.log(err)
     });
 })
-
-
-
-
 
 router.get('/wish-list', checkForAuth ,(req, res) => {
   const layout = req.user ? '/layout/auth' : '/layout/noAuth'
@@ -69,7 +71,6 @@ router.get('/wish-list', checkForAuth ,(req, res) => {
     console.log(err)
   });
 })
-
 router.post('/wish-list/:_id', checkForAuth, (req,res,next)=>{
   User.findById(req.user._id)
   .then((result) => {
@@ -86,7 +87,55 @@ router.post('/wish-list/:_id', checkForAuth, (req,res,next)=>{
     console.log(err)
   });
 })
+router.post('/wish-list/:_id/delete', checkForAuth ,(req,res)=>{
+  User.findByIdAndUpdate(req.user._id, {$pull: {wishList: req.params._id}})
+  .then((result) => {
+    res.redirect('/profile/wish-list')
+  }).catch((err) => {
+    console.log(err)
+  });
+})
 
-
-
+router.get('/sales-list', checkForAuth, (req,res)=>{
+  const layout = req.user ? '/layout/auth' : '/layout/noAuth'
+  User.findById(req.user._id)
+  .populate('salesList')
+  .then((result) => {
+    res.render('profile/salesList', {user: result, layout: layout})
+  }).catch((err) => {
+    console.log(err)
+  });
+})
+router.post('/sales-list/:_id', checkForAuth, (req,res)=>{
+  User.findById(req.user._id)
+  .then((result) => {
+    if(result.myList.includes(req.params._id) && !result.salesList.includes(req.params._id)){
+      User.findByIdAndUpdate(req.user._id, {$push: {salesList: req.params._id}})
+      .then((result) => {
+        res.redirect('/profile/sales-list')
+      })
+    }else{
+      res.redirect('/profile/sales-list')
+    }
+  }).catch((err) => {
+    console.log(err)
+  });
+})
+router.post('/sales-list/:_id/delete', checkForAuth,(req,res)=>{
+  User.findByIdAndUpdate(req.user._id, {$pull: {salesList: req.params._id}})
+  .then((result) => {
+    res.redirect('/profile/sales-list')
+  }).catch((err) => {
+    console.log(err)
+  });
+})
+router.post('/sales-list/:_id/sold', checkForAuth, (req,res)=>{
+  User.findByIdAndUpdate(req.user._id, {$pull: {salesList: req.params._id, myList: req.params._id}})
+  .then((result) => {
+    res.redirect('/profile/sales-list')
+  })
+  .catch((err) => {
+    console.log(err)
+  });
+})
 module.exports = router;
